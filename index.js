d5e45164f46b1f23ab05437178fc68158ba9de7a
Rafael Verger <rafael@rafaelverger.com.br>
2020-10-31 23:06:07 +0000

    Adds nunjucks and index template

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 
 const app = express();
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || '0.0.0.0';
 
+nunjucks.configure('server/templates/', {
+  express: app,
+  autoescape: true,
+  watch: true,
+});
+
 app.get('/', (_, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.render('index.njk');
 });
 
 app.listen(port, host, () => console.log(`Listening on ${host}:${port}`));
